aa2079613a56518e82d44ed16654ae4521147e08
nic netzel and yvonna contreras <netzeln@gmail.com>
2016-01-25 10:11:44 -0800

    Add append feature for New Contacts

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,25 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
-// $(document).ready(function() {
-//
-//
-//
-// });
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>")
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+  $(".contact").last().click(function(){
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.fullName());
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+
+});
+});
