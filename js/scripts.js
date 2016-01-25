00ce44562d3d14cfb4999066ff16be375116e2c9
nic netzel and yvonna contreras <yvonnap.contreras@gmail.com>
2016-01-25 11:12:07 -0800

    Add additional address fields

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,22 @@ Address.prototype.fullAddress = function () {
 }
 
 $(document).ready(function() {
+  $("#add-address").click(function(){
+    $("#new-addresses").append('<div class="new-addresses">'+
+                                '<div class="form-group">'+
+                                  '<label for="new-street">Street</label>' +
+                                  '<input type="text" class="form-control new-street" >' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-city">City</label>' +
+                                  '<input type="text" class="form-control new-city">' +
+                                '</div>' +
+                                '<div class="form-group">' +
+                                  '<label for="new-state">State</label>' +
+                                  '<input type="text" class="form-control new-state">' +
+                                '</div>' +
+                              '</div>');
+  });
   $("form#new-contact").submit(function(event){
     event.preventDefault();
 
